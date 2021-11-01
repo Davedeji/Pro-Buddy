4d1e4cb8f9feba7e16ed3c57c5c84d1a36078676
Shuang Wu <shuang111994@gmail.com>
2021-10-31 22:46:53 -0700

    Restructure file locations

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA7CV3TMTkasLK9leGD6aMMywkAQQOUXdM",
+    authDomain: "pro-buddy.firebaseapp.com",
+    projectId: "pro-buddy",
+    storageBucket: "pro-buddy.appspot.com",
+    messagingSenderId: "55992554126",
+    appId: "1:55992554126:web:f331f390ea11cb09836fdf"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
