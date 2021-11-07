fbc7fc6fa137138df92246921f06c79fdc4786fe
Niki Guo <qguo20@my.bcit.ca>
2021-11-06 21:12:01 -0700

    Add login_niki and added universal.js file

---
+++ scripts/universal.js
@@ -0,0 +1,55 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      var user = authResult.user;
+
+      //determine whether the user is a new user or not
+      if (authResult.additionalUserInfo.isNewUser) {
+        db.collection("Users")
+          .doc(user.uid)
+          .set({
+            name: user.displayName,
+            email: user.email,
+          })
+          .then(function () {
+            window.location.assign("main.html");
+          })
+          .catch(function (error) {
+            console.log("error is" + error);
+          });
+      } else {
+        return true;
+      }
+      return false;
+    },
+    uiShown: function () {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById("loader").style.display = "none";
+    },
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: "popup",
+  signInSuccessUrl: "main.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url.
+  privacyPolicyUrl: "<your-privacy-policy-url>",
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
