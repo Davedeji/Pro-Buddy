3ab2e9bcb94fdb18aad86329975011876d87c423
Adedeji Toki <adedeji.toki@outlook.com>
2021-11-11 23:28:57 -0800

    Update chooseSkills and chooseInterests to read from firestore

--- scripts/skillsInterestsHelper.js
+++ scripts/skillsInterestsHelper.js
@@ -22,7 +22,42 @@ function cardsSelected(firestoreDocID) {
     continueClicked(selectedSkills, firestoreDocID)
 }
 
+function retrieveSelectedCards(firestoreDocID) {
+    console.log("retrieveing doc")
+    var docRef = db.collection("users").doc("user03").collection("userChoices").doc(firestoreDocID);
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            // if doc.data().
+            const cards = doc.data()
+            for (var field in doc.data()) {
+                console.log(field)
+                console.log(cards[field])
+
+                if (cards[field]) {
+                    var elem = document.getElementById(field);
+                    elem.classList.toggle('selected')
+                    console.log('card selected')
+                }
+                
+
+            }
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
 function continueClicked(data, firestoreDocID) {
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+        console.log(user.uid)
+    }
     db.collection("users").doc("user03").collection("userChoices").doc(firestoreDocID).set(data)
         .then(() => {
             console.log("Document successfully written!");
