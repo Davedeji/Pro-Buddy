2d125343f228ab35bb74f708693044caa1b06ebb
Shuang Wu <shuang111994@gmail.com>
2021-12-01 11:57:36 -0800

    Comment uploadOverview.js

--- scripts/uploadOverview.js
+++ scripts/uploadOverview.js
@@ -1,37 +1,32 @@
+// Display all uploads retrieved from database
 function displayCards(collection) {
-    let CardTemplate = document.getElementById("CardTemplate");
+  let CardTemplate = document.getElementById("CardTemplate");
+  // check if current user is signed in
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      // query through uploads collection to get current user's uploads
+      db.collection("uploads").where("ID", "==", user.uid)
+        .get().then(snap => {
+          // record data for all uploads that meet query specification
+          snap.forEach(doc => {
+            var title = doc.data().title;
+            var description = doc.data().description;
+            var date = doc.data().date.toDate().toDateString();
+            var download = doc.data().download;
+            // create a card using existing template
+            let newcard = CardTemplate.content.cloneNode(true);
 
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        db.collection("uploads").where("ID", "==", user.uid)
-          .get().then(snap => {
-            snap.forEach(doc => {
-              var title = doc.data().title;
-              var description = doc.data().description;
-              var date = doc.data().date.toDate().toDateString();
-              var download = doc.data().download;
-              let newcard = CardTemplate.content.cloneNode(true);
+            // insert data into card
+            newcard.querySelector('.card-title').innerHTML = title;
+            newcard.querySelector('#uploadDate').innerHTML = date;
+            newcard.querySelector('#uploadDescription').innerHTML = description;
+            newcard.querySelector('.btn').setAttribute("href", download);
 
-
-
-
-
-              //update title and text and image
-              newcard.querySelector('.card-title').innerHTML = title;
-              newcard.querySelector('#uploadDate').innerHTML = date;
-              newcard.querySelector('#uploadDescription').innerHTML = description;
-              newcard.querySelector('.btn').setAttribute("href", download);
-
-              //attach to gallery
-              document.getElementById("uploads-go-here").appendChild(newcard);
-            })
-
-
-
-
+            //attach to container
+            document.getElementById("uploads-go-here").appendChild(newcard);
           })
-
-      }
-    })
-  }
-  displayCards();
+        })
+    }
+  })
+}
+displayCards();
